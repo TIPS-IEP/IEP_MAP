a4d42ef27f6335bd0e0dde9abe7bd2bef9614084
Alanhou1222 <alanhou911222@gmail.com>
2021-05-20 11:42:05 +0800

    wtf

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,8 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("myMap"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 0.2,
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -3.397, lng: 150.644 },
+    zoom: 8,
   });
 }
