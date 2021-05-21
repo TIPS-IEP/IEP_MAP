2a70f56c942363472f28de362c904e31ad16bc56
Alanhou1222 <alanhou911222@gmail.com>
2021-05-21 16:01:19 +0800

    test git

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,12 +8,12 @@ function initMap() {
   });
   var georssLayer = new google.maps.KmlLayer({
     url: 'https://www.google.com/maps/d/u/0/kml?mid=1sItIYWAALKvp2787yj9uxeYB3rQGoJi1'
-    zoom: 4,
-    center: {lat: 40.496675, lng: -96.65625}
   });
   georssLayer.setMap(map);
   // var map = new google.maps.Map(document.getElementById('map'), {
   //   zoom: 4,
   //   center: {lat: 40.496675, lng: -96.65625}
   // });
+
+  //
 }
