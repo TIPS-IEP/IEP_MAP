d2e4fcfc99a744a2c9d4e139e0c71ea979a8d86a
Alanhou1222 <alanhou911222@gmail.com>
2021-05-22 16:58:25 +0800

    add marker (size issue)

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,14 +6,26 @@ function initMap() {
     zoom: 4,
     center: {lat: 40.496675, lng: -96.65625}
   });
-  var georssLayer = new google.maps.KmlLayer({
-    url: 'https://www.google.com/maps/d/u/0/kml?mid=1sItIYWAALKvp2787yj9uxeYB3rQGoJi1'
-  });
-  georssLayer.setMap(map);
-  // var map = new google.maps.Map(document.getElementById('map'), {
-  //   zoom: 4,
-  //   center: {lat: 40.496675, lng: -96.65625}
-  // });
+const icons = {
+  UofM: {
+    icon: "images/UofM.png",
+    
+  },
+};
+const features = [
+  {
+    position: new google.maps.LatLng(42.27656, -83.74256),
+    type: "UofM",
+  },
+];
 
-  //
+// Create markers.
+for (let i = 0; i < features.length; i++) {
+  const marker = new google.maps.Marker({
+    position: features[i].position,
+    icon: icons[features[i].type].icon,
+    scaledSize : new google.maps.Size(2, 30),
+    map: map,
+  });
+}
 }
