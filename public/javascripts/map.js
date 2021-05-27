8d26389c610bbe52598034e49479843664c0e828
Alanhou1222 <alanhou911222@gmail.com>
2021-05-27 15:19:56 +0800

    fix google map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,26 +6,29 @@ function initMap() {
     zoom: 4,
     center: {lat: 40.496675, lng: -96.65625}
   });
-const icons = {
-  UofM: {
-    icon: "images/UofM.png",
-    
-  },
-};
-const features = [
-  {
-    position: new google.maps.LatLng(42.27656, -83.74256),
-    type: "UofM",
-  },
-];
+  var image = {
+    url: "images/universities/UofM.png",
+    scaledSize: new google.maps.Size(30,32)
+  };
+  const icons = {
+    UofM: {
+      icon: image,
+      
+    },
+  };
+  const features = [
+    {
+      position: new google.maps.LatLng(42.27656, -83.74256),
+      type: "UofM",
+    },
+  ];
 
-// Create markers.
-for (let i = 0; i < features.length; i++) {
-  const marker = new google.maps.Marker({
-    position: features[i].position,
-    icon: icons[features[i].type].icon,
-    scaledSize : new google.maps.Size(2, 30),
-    map: map,
-  });
-}
+  // Create markers.
+  for (let i = 0; i < features.length; i++) {
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
+      map: map,
+    });
+  }
 }
