cdcd91f3adba9d1ffefd1aeaac161de8c2083d02
Alanhou1222 <alanhou911222@gmail.com>
2021-05-30 14:18:55 +0800

    Revert "replace sheets"
    This reverts commit 6f4ef955c75f44627a1ab0f4e95d64e8cc67eac1.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,73 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
 
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById("navbar").style.padding = "8px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.padding = "20px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+const users = []
+//Alumni
+var Alumni = require('../models/Alumni');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET map page. */
+router.get('/map', function(req, res, next) {
+  res.render('map');
+});
+
+/* GET mongodb test pages. */
+router.get('/add-alumni', (req,res) => {
+  const alumni = new Alumni({
+    EnglishName:'Alan',
+    Email:'alanhou911222@gmail.com',
+    InstagramUsername:'alanhou.987',
+    GraduationYear: '2021',
+    Major: 'Computer Science'
+  });
+  alumni.save()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+router.get('/all-alumni', (req,res) => {
+  Alumni.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+router.get('/findalan', (req,res) => {
+  Alumni.findById('60adf1cf39218d31133e8659')
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get(
+  '/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }), 
+  (req, res) => {
+    res.redirect('/')
   }
-}
+);
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
+
+
+module.exports = router;
+
