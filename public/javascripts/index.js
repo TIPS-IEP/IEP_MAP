7b9ae532434198343882c4aa03077fc4347b7243
Andrew Chuang <andr3wchuang@berkeley.edu>
2021-06-01 15:01:11 +0800

    refine our features, contact us pages

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,10 +3,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById("navbar").style.padding = "8px 10px";
+    document.getElementById("navbar").style.padding = "7px 10px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
-    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("navbar").style.padding = "25px 10px";
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
