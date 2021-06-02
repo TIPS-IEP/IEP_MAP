ec8672cb235074a019c1f3358a40da4508b36768
Andrew Chuang <andr3wchuang@berkeley.edu>
2021-06-02 15:38:27 +0800

    finish sidenav

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,21 +5,15 @@ function scrollFunction() {
   if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
     document.getElementById("navbar").style.padding = "7px 10px";
     document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("z-index").style.fontSize = "32px";
+
   } else {
     document.getElementById("navbar").style.padding = "25px 10px";
     document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("z-index").style.fontSize = "33px";
+
   }
 }
-
-// /* Set the width of the side navigation to 250px */
-// function openNav() {
-//   document.getElementById("mySidenav").style.width = "250px";
-// }
-
-// /* Set the width of the side navigation to 0 */
-// function closeNav() {
-//   document.getElementById("mySidenav").style.width = "0";
-// }
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
