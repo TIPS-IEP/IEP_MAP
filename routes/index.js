0a913316d2f191826a84d60463b7738c599ab329
Alanhou1222 <alanhou911222@gmail.com>
2021-06-04 14:11:16 +0800

    willy required too

--- routes/index.js
+++ routes/index.js
@@ -105,6 +105,39 @@ router.get('/user', ensureAuth, (req, res) => {
 
 router.get('/admin', ensureAuth, async (req, res) => {
   try{
+    
+      var x = 50;
+      var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: {lat: x, lng: -96.65625}
+      });
+
+      var features = [10];
+      features[0]={
+          position: new google.maps.LatLng(42.27656, -83.74256),
+          type: "UniversityofMichigan",
+      }
+
+      var image = {
+          url: "images/universities/University of Michigan.png",
+          scaledSize: new google.maps.Size(30,32)
+      };
+      const icons = {
+          UniversityofMichigan: {
+          icon: image,
+          },
+      };
+      // Create markers.
+      for (let i = 0; i < features.length; i++) {
+          const marker = new google.maps.Marker({
+          position: features[i].position,
+          icon: icons[features[i].type].icon,
+          map: map,
+          });
+      }
+  
+  
+  
     const items = await Universities.find().lean()
     console.log(items)
     res.render('admin', {
