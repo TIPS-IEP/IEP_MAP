a2203067936b15e4600fe91632e80ea28388597a
magician3124 <crashingballoon@gmail.com>
2021-06-06 16:23:26 +0800

    change navbar

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,12 +6,13 @@ function scrollFunction() {
     document.getElementById("navbar").style.padding = "7px 10px";
     document.getElementById("logo").style.fontSize = "25px";
     document.getElementById("z-index").style.fontSize = "32px";
+    document.getElementById("navbar").style.opacity = "80%";
 
   } else {
     document.getElementById("navbar").style.padding = "25px 10px";
     document.getElementById("logo").style.fontSize = "35px";
     document.getElementById("z-index").style.fontSize = "33px";
-
+    document.getElementById("navbar").style.opacity = "100%";
   }
 }
 
