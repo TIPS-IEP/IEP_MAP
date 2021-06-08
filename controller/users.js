1f71a763d560be6d0944c84f9aa04226ea4c5196
magician3124 <crashingballoon@gmail.com>
2021-06-08 16:06:21 +0800

    data added

--- controller/users.js
+++ controller/users.js
@@ -1,17 +1,35 @@
+//database
+var mongoose = require('mongoose');
+var Alumni = require('../models/Alumni');
+var Universities = require('../models/Universities')
+
 exports.logout = function(req, res) {
     req.logout()
     res.redirect('/')
 }
 
-exports.showProfile = function(req, res) {
+exports.showProfile = async function(req, res) {
+    const users = await Alumni.find({ Email: req.user.email }).lean()
+    var data = []
+    users.forEach(function(item){
+        data.push(item.EnglishName);
+        data.push(item.Email);
+        data.push(item.InstagramUsername);
+        data.push(item.GraduationYear);
+        data.push(item.Major);
+    });
     res.render('login/profile', {
-      name: req.user.firstName,
-      picture: req.user.image,
+        name: req.user.firstName,
+        picture: req.user.image,
+        data: data
+        // instagramUsername: user.instagramUsername,
+        // graduationYear: user.graduationYear,
+        // major: user.major
     });
 }
 
 exports.showUser = function(req, res) {
     res.render('login/access', {
-      name: req.user.firstName,
+        name: req.user.firstName,
     });
 }
