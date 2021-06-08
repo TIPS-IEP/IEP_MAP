d276c35737413c3eb566f2f52394612b771cb33c
Alanhou1222 <alanhou911222@gmail.com>
2021-06-08 14:59:17 +0800

    reload bug not fix

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ router.get('/user', ensureAuth, usersController.showUser)
 router.get('/profile', ensureAuth, usersController.showProfile)
 
 // GET map page
-router.get('/map', ensureAuth, ensureSuper, mapController.showMap);
+router.get('/map', mapController.showMap);
 
 module.exports = router;
 
