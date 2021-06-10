4ad9051eab3f7995f23162f04beafc1c129edc02
Alanhou1222 <alanhou911222@gmail.com>
2021-06-10 16:13:01 +0800

    map add alumni

--- controller/map.js
+++ controller/map.js
@@ -9,10 +9,27 @@ exports.showMap = async function(req, res) {
     var universityName = [];
     var universityLat = [];
     var universityLng = [];
+    var alumnItem = await Alumni.find().lean();
+    var aLength = alumnItem.length;
+    var alumniEnglishName = [];
+    var alumniUniversity = [];
+    var alumniEmail = [];
+    var alumniInstagramUsername = [];
+    var alumniGraduationYear = [];
+    var alumniMajor = [];
+    console.log(alumnItem);
     uniItems.forEach(function(item){
         universityName.push(item.university);
         universityLat.push(item.lat);
         universityLng.push(item.lng);
+    });
+    alumnItem.forEach(function(item){
+        alumniEnglishName.push(item.EnglishName);
+        alumniUniversity.push(item.University);
+        alumniEmail.push(item.Email);
+        alumniInstagramUsername.push(item.InstagramUsername);
+        alumniGraduationYear.push(item.GraduationYear);
+        alumniMajor.push(item.Major);
     });
 
     if(req.isAuthenticated()){
@@ -26,6 +43,13 @@ exports.showMap = async function(req, res) {
         universityName: universityName,
         universityLat: universityLat,
         universityLng: universityLng,
+        aLength: aLength,
+        alumniEnglishName: alumniEnglishName,
+        alumniUniversity: alumniUniversity,
+        alumniEmail: alumniEmail,
+        alumniInstagramUsername: alumniInstagramUsername,
+        alumniGraduationYear: alumniGraduationYear,
+        alumniMajor: alumniMajor,
         loggedin: req.isAuthenticated(),
         name: firstName,
     });
