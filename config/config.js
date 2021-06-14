9bdda3ef52811821f36eba2aed6e9418d163da69
magician3124 <crashingballoon@gmail.com>
2021-06-14 14:04:21 +0800

    test deploy

--- config/config.js
+++ config/config.js
@@ -23,5 +23,5 @@ module.exports = {
   },
   clientId: "1061733762911-t88po8vnhmtp3tvfdhcblhoshinbunbi.apps.googleusercontent.com",
   secret: "g25x6ba7U6buClbk60_p-gkz",
-  callback: 'http://localhost:3000/auth/google/callback'
+  callback: 'https://iepmap.herokuapp.com/auth/google/callback'
 }
