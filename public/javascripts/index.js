3a27a9a7d8f84aebdc52dad89318d66864160b1a
Andrew Chuang <andr3wchuang@berkeley.edu>
2021-06-14 14:43:05 +0800

    change side bar

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -40,7 +40,7 @@ function closeNav() {
 /* Control z-index*/
 window.onload = function displayzIndex(){
   var path = window.location.pathname.toString();
-  if(path == "/contact"){
+  if(path == "/contact" || path == "/about" || path == "/board" || path == "/events" || path == "/share"){
     document.getElementById("z-index").style.display = "none";
   }
   else{
