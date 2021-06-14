7855e4a6f60a9c5e1e04c5997dd817a303ef90ca
Alanhou1222 <alanhou911222@gmail.com>
2021-06-14 14:38:35 +0800

    move z- index control to nav.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -38,13 +38,5 @@ function closeNav() {
   document.getElementById("hide4").style.display = "block";
 }
 /* Control z-index*/
-window.onload = function displayzIndex(){
-  var path = window.location.pathname.toString();
-  if(path == "/contact"){
-    document.getElementById("z-index").style.display = "none";
-  }
-  else{
-    document.getElementById("z-index").style.display= "contents";
-  }
-}
+
 
