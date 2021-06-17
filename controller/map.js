95b7b42b42666931b8457ea1b1a1c2b29841cdf8
magician3124 <crashingballoon@gmail.com>
2021-06-17 20:42:22 +0800

    map bug fixed

--- controller/map.js
+++ controller/map.js
@@ -3,52 +3,56 @@ var mongoose = require('mongoose');
 var Alumni = require('../models/Alumni');
 var Universities = require('../models/Universities')
 
+class universitiesClass {
+    constructor(universities) {
+        this.universityName = new Array();
+        this.universityLat = new Array();
+        this.universityLng = new Array();
+        this.universitiesLength = universities.length;
+        for(var i = 0; i < this.universitiesLength; i++){
+            this.universityName.push(universities[i].university);
+            this.universityLat.push(universities[i].lat);
+            this.universityLng.push(universities[i].lng);
+        }
+    }
+}
+
+class alumniClass {
+    constructor(alumni) {
+        this.alumniEnglishName = new Array();
+        this.alumniUniversity = new Array();
+        this.alumniEmail = new Array();
+        this.alumniInstagramUsername = new Array();
+        this.alumniGraduationYear = new Array();
+        this.alumniMajor = new Array();
+        this.alumniLength = alumni.length;
+        for(var i = 0; i < this.alumniLength; i++){
+            this.alumniEnglishName.push(alumni[i].EnglishName);
+            this.alumniUniversity.push(alumni[i].University);
+            this.alumniEmail.push(alumni[i].Email);
+            this.alumniInstagramUsername.push(alumni[i].InstagramUsername);
+            this.alumniGraduationYear.push(alumni[i].GraduationYear);
+            this.alumniMajor.push(alumni[i].Major);
+        }
+    }
+}
+
+
 exports.showMap = async function(req, res) {
-    var uniItems = await Universities.find().lean();
-    var length = uniItems.length;
-    var universityName = [];
-    var universityLat = [];
-    var universityLng = [];
-    var alumnItem = await Alumni.find().lean();
-    var aLength = alumnItem.length;
-    var alumniEnglishName = [];
-    var alumniUniversity = [];
-    var alumniEmail = [];
-    var alumniInstagramUsername = [];
-    var alumniGraduationYear = [];
-    var alumniMajor = [];
-    uniItems.forEach(function(item){
-        universityName.push(item.university);
-        universityLat.push(item.lat);
-        universityLng.push(item.lng);
-    });
-    alumnItem.forEach(function(item){
-        alumniEnglishName.push(item.EnglishName);
-        alumniUniversity.push(item.University);
-        alumniEmail.push(item.Email);
-        alumniInstagramUsername.push(item.InstagramUsername);
-        alumniGraduationYear.push(item.GraduationYear);
-        alumniMajor.push(item.Major);
-    });
+    var universities = await Universities.find().lean();
+    var alumni = await Alumni.find().lean();
+    
+    const universitiesArranged = new universitiesClass(universities);
+    const alumniArranged = new alumniClass(alumni);
 
     if(req.isAuthenticated()){
         var firstName = req.user.firstName;
     }else{
         var firstName = null
     }
-
     res.render('map',{
-        length: length,
-        universityName: universityName,
-        universityLat: universityLat,
-        universityLng: universityLng,
-        aLength: aLength,
-        alumniEnglishName: alumniEnglishName,
-        alumniUniversity: alumniUniversity,
-        alumniEmail: alumniEmail,
-        alumniInstagramUsername: alumniInstagramUsername,
-        alumniGraduationYear: alumniGraduationYear,
-        alumniMajor: alumniMajor,
+        universities: universitiesArranged,
+        alumni: alumniArranged,
         loggedin: req.isAuthenticated(),
         name: firstName,
     });
