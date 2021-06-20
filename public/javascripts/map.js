1362e7e2b4c43a1995e0001c51e6922b31c7949c
Alanhou1222 <alanhou911222@gmail.com>
2021-06-20 15:30:55 +0800

    refine map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,13 +1,11 @@
 /* Set the width of the sidebar to 250px (show it) */
 function opensideShow() {
-  document.getElementById("sideShow").style.width = "300px";
-  document.getElementById("map").style.marginLeft = "300px";
-  document.getElementById("allUniversity").style.width = "0px";
+  document.getElementById("sideShow").style.display = "inline-block";
+  document.getElementById("allUniversity").style.display = "none";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closesideShow() {
-  document.getElementById("sideShow").style.width = "0";
-  document.getElementById("map").style.marginLeft = "0";
-  document.getElementById("allUniversity").style.width = "30%";
+  document.getElementById("sideShow").style.display = "none";
+  document.getElementById("allUniversity").style.display = "inline-block";
 }
