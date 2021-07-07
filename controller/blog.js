d456d0a5e26aadf97b14963aecf3fbec33232b32
magician3124 <crashingballoon@gmail.com>
2021-07-07 15:53:33 +0800

    blog finish

--- controller/blog.js
+++ controller/blog.js
@@ -1,8 +1,9 @@
+
 var Blog = require("../models/blog")
 
 exports.addBlog = async function(req, res, next) {
   try {
-    req.body.user = req.user.id
+    req.body.email = req.user.email
     await Blog.create(req.body)
     res.redirect("/blog")
   } catch (error) {
@@ -13,7 +14,7 @@ exports.addBlog = async function(req, res, next) {
 
 exports.showDashboard = async function(req, res, next) {
   var isAuthenticated = false;
-  const blogs = await Blog.find({ user: req.user.id }).lean();
+  const blogs = await Blog.find({ email: req.user.email }).lean();
   if(req.isAuthenticated()){
     var firstName = req.user.firstName;
     isAuthenticated = true;
@@ -27,7 +28,36 @@ exports.showDashboard = async function(req, res, next) {
   });
 }
 
+exports.editBlog = async function(req, res, next) {
+  var isAuthenticated = false;
+  if(req.isAuthenticated()){
+    var firstName = req.user.firstName;
+    isAuthenticated = true;
+  }else{
+    var firstName = null
+  }
+  var blog = await Blog.find({blogId: req.params.blog_id, email: req.user.email }).lean();
+  if(!blog){
+    res.render('error')
+  }else{
+    res.render('blog/editBlog', {
+      loggedin: isAuthenticated,
+      name: firstName,
+      blog: blog,
+    });
+  }
+}
 
-// exports.showEachBlog = async function(req, res, next) {
-  
-// }
+exports.saveBlog = async function(req, res, next) {
+  req.body.email = req.user.email;
+  console.log(req.params.blog_id)
+  await Blog.deleteOne({blogId: req.params.blog_id}, function(err, obj) {
+    if (err) throw err;
+    console.log("delete blog " + req.params.blog_id + " from blog");
+  });
+  await Blog.create(req.body, function(err, obj) {
+      if (err) throw err;
+      console.log("create blog " + req.params.blog_id + " in blog");
+  });
+  res.redirect("/dashboard")
+}
