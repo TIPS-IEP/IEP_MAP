ff62464144bdebc141b7dc2714a3ace5e60cffea
Alanhou1222 <alanhou911222@gmail.com>
2021-07-12 15:39:53 +0800

    add loading

--- public/javascripts/newIndex.js
+++ public/javascripts/newIndex.js
@@ -1,1 +1,10 @@
-
+window.onload = function() {
+  document.getElementsByClassName("animated-title")[0].setAttribute("style", "display:none;");
+  var heroImg = new Image();
+  heroImg.src = "images/hero/cover1.jpg";
+  heroImg.onload = function(){
+    document.getElementsByClassName("hero-image1")[0].style.opacity = "1";
+    document.getElementsByClassName("animated-title")[0].setAttribute("style", "display:block;");
+    document.getElementsByClassName("loadingDots")[0].style.display = "none";
+  }
+}
