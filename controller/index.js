06be8d3a4dfe245827c9796f5e8e829b2e301087
magician3124 <crashingballoon@gmail.com>
2021-07-19 16:24:08 +0800

    bug half fix

--- controller/index.js
+++ controller/index.js
@@ -1,42 +1,66 @@
+var isAuthenticated = false;
+var image = null;
+var firstName = null;
+
+function importData(){
+  if(req.isAuthenticated()){
+    firstName = req.user.firstName;
+    isAuthenticated = true;
+    image = req.user.image;
+  }else{
+    firstName = null;
+    isAuthenticated = false;
+    image = null;
+  }
+  console.log(firstName);
+  console.log(isAuthenticated);
+  console.log(image);
+}
+
 exports.index = function(req, res, next) {
-    var isAuthenticated = false;
-    if(req.isAuthenticated()){
-      var firstName = req.user.firstName;
-      isAuthenticated = true;
-    }else{
-      var firstName = null
-    }
-    res.render('index', {
-      loggedin: isAuthenticated,
-      name: firstName,
-    });
+  if(req.isAuthenticated()){
+    firstName = req.user.firstName;
+    isAuthenticated = true;
+    image = req.user.image;
+  }else{
+    firstName = null;
+    isAuthenticated = false;
+    image = null;
+  }
+  res.render('index', {
+    loggedin: isAuthenticated,
+    name: firstName,
+    image: image,
+  });
 }
 
 exports.newIndex = function(req, res, next) {
-  var isAuthenticated = false;
   if(req.isAuthenticated()){
-    var firstName = req.user.firstName;
+    firstName = req.user.firstName;
     isAuthenticated = true;
+    image = req.user.image;
   }else{
-    var firstName = null
+    firstName = null;
+    isAuthenticated = false;
+    image = null;
   }
   res.render('newIndex', {
     loggedin: isAuthenticated,
     name: firstName,
-    image: req.user.image,
+    image: image,
   });
 }
 
 exports.showAbout = function(req, res) {
-    if(req.isAuthenticated()){
-      var firstName = req.user.firstName;
-    }else{
-      var firstName = null
-    }
-    res.render('about/about', {
-      loggedin: req.isAuthenticated(),
-      name: firstName,
-    });
+  if(req.isAuthenticated()){
+    var firstName = req.user.firstName;
+  }else{
+    var firstName = null
+  }
+  res.render('about/about', {
+    loggedin: req.isAuthenticated(),
+    name: firstName,
+  });
 }
 
 exports.showContact = function(req, res) {
