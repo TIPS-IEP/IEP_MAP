0307ec41b1cd28eb26cd2bf27d36b6300892a0f6
magician3124 <crashingballoon@gmail.com>
2021-07-22 15:31:35 +0800

    navbar line bug fixed

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -1,3 +1,6 @@
+var hover = '.dropdown:hover{ margin-bottom: 13px; padding-bottom: 3px; border-bottom: 4px solid rgb(255, 179, 37);}';
+
+
 $(document).ready(function() {
   var path = window.location.pathname.toString();
   if (screen.width > 600) {
@@ -15,28 +18,38 @@ window.addEventListener('scroll', function () {
   let logoImage = document.getElementsByClassName("logo-image")
   let navLogo = document.getElementsByClassName("nav-logo")
   let navOption = document.getElementsByClassName("navOption")
+  let allDropdown = document.getElementsByClassName("allDropdown")
+  let allDropdownContent = document.getElementsByClassName("all-dropdown-content")
   if (window.pageYOffset > 40) {
-    // document.getElementById("navbar").classList.add('bg-light', 'shadow');
     document.getElementById("navbar").style.background = "rgb(29, 29, 29)";
     document.getElementById("navbar").style.padding = "7px 10px";
     document.getElementById("navbar-right").style.fontSize = "25px";
     document.getElementById("navbar").style.opacity = "85%";
     for (let i = 0; i < $('.navOption').length; i++) {
       navOption[i].setAttribute("style", "color: rgb(255, 255, 255)!important;");
-        // highlight the mouseover target
-        // navOption[i].setAttribute("style", "color: rgb(0, 0, 0)!important;");
-        // event.target.style.color = "black";
     } 
     for (let i = 0; i < $('.navOption').length; i++) {
       navOption[i].addEventListener("mouseover", function( event ) {
-        // highlight the mouseover target
         navOption[i].setAttribute("style", "color: rgb(255, 179, 37) !important;");
       }, false);
       navOption[i].addEventListener("mouseout", function( event ) {
-        // highlight the mouseover target
         navOption[i].setAttribute("style", "color: white !important;");
       }, false);
     }
+
+    for (let i = 0; i < $('.all-dropdown-content').length; i++) {
+      allDropdownContent[i].setAttribute("style", "margin-top: 59px;");
+    }
+
+    for (let i = 0; i < $('.allDropdown').length; i++) {
+      allDropdown[i].addEventListener("mouseover", function( event ) {
+        allDropdown[i].setAttribute("style", "margin-bottom: 13px; padding-bottom: 3px; border-bottom: 4px solid rgb(255, 179, 37);");
+      }, false);
+      allDropdown[i].addEventListener("mouseout", function( event ) {
+        allDropdown[i].setAttribute("style", "margin-bottom: 13px; padding-bottom: 7px;");
+      }, false);
+    }
+
     for (let i = 0; i < $('.logo-image').length; i++) {
       logoImage[i].setAttribute("style", "content: url('/images/logos/TIPS-logo6/cover.png')!important;");
     }
@@ -53,18 +66,26 @@ window.addEventListener('scroll', function () {
       document.getElementById("navbar").style.background = "none";
       for (let i = 0; i < $('.navOption').length; i++) {
         navOption[i].setAttribute("style", "color: rgb(0, 0, 0)!important;");
-          // highlight the mouseover target
-          // navOption[i].setAttribute("style", "color: rgb(255, 255, 255)!important;");
-          // event.target.style.color = "white";
         navOption[i].addEventListener("mouseover", function( event ) {
-          // highlight the mouseover target
           event.target.style.color = "white";
         }, false);
         navOption[i].addEventListener("mouseout", function( event ) {
-          // highlight the mouseover target
           event.target.style.color = "";
         }, false);
       }
+      for (let i = 0; i < $('.allDropdown').length; i++) {
+        allDropdown[i].addEventListener("mouseover", function( event ) {
+          allDropdown[i].setAttribute("style", "margin-bottom: 0px; padding-bottom: 16px; border-bottom: 4px solid rgb(255, 179, 37);");
+        }, false);
+        allDropdown[i].addEventListener("mouseout", function( event ) {
+          allDropdown[i].setAttribute("style", "margin-bottom: 0px; padding-bottom: 20px;");
+        }, false);
+      }
+
+      for (let i = 0; i < $('.all-dropdown-content').length; i++) {
+        allDropdownContent[i].setAttribute("style", "margin-top: 72px;");
+      }
+
       for (let i = 0; i < $('.logo-image').length; i++) {
         logoImage[i].setAttribute("style", "content: url('/images/logos/TIPS-logo7/cover.png')!important;");
       }
@@ -82,10 +103,8 @@ window.addEventListener('scroll', function () {
   }
 });
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   if (screen.width < 600) {
-  /* Open the sidenav */
     document.getElementById("mySidenav").style.height = "100vh";
     document.getElementById("mySidenav").style.paddingTop = "20vh";
     document.getElementById("mySidenav").style.width = "100vw";
@@ -106,7 +125,6 @@ function openNav() {
   }
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   if (screen.width < 600) {
     document.getElementById("mySidenav").style.height = "0";
@@ -128,10 +146,6 @@ function closeNav() {
   }
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
 var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
 function preventDefault(e) {
@@ -145,7 +159,6 @@ function preventDefaultForScrollKeys(e) {
   }
 }
 
-// modern Chrome requires { passive: false } when adding event
 var supportsPassive = false;
 try {
   window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
@@ -159,7 +172,6 @@ var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewh
 
 
 function mouseDown() {
-  // document.getElementsByClassName("nav-div")[0].style.backgroundColor = "white";
   document.getElementsByClassName("nav-div")[0].setAttribute("style", "box-shadow: 0.1rem 0.15rem 0.3rem rgb(83 88 93 / 80%) !important;");
 
   if(document.getElementById("sidenavSeperateLine")){
@@ -176,7 +188,6 @@ function mouseUp() {
 }
 
 
-// call this to Disable
 function disableScroll() {
   window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
   window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
@@ -184,7 +195,6 @@ function disableScroll() {
   window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
-// call this to Enable
 function enableScroll() {
   window.removeEventListener('DOMMouseScroll', preventDefault, false);
   window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
